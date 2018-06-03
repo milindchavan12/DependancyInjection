f3f36ddd5f94bd98d5774385539efd6a9b597c13
Milind Chavan <Milind@Milinds-MacBook-Air.local>
2018-06-03 22:30:04 +0200

    With Code

---
+++ DependancyInjection/PersonRepository.Interface/IPersonRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using SharedObjects;
+
+namespace PersonRepository.Interface
+{
+    public interface IPersonRepository
+    {
+        IEquatable<Person> GetPeople();
+
+        Person GetPerson(string lastName);
+
+        void AddPerson(Person newPerson);
+
+        void UpdatePerson(string lastName, Person updatedPerson);
+
+        void DeletePerson(string lastName);
+
+        void UpdatePeople(IEquatable<Person> updatedPeople);
+    }
+}
---
+++ DependancyInjection/PersonRepository.Interface/PersonRepository.Service/ServiceRepository.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using PersonRepository.Interface;
+using SharedObjects;
+
+namespace PersonRepository.Service
+{
+    public class ServiceRepository : IPersonRepository
+    {
+        public void AddPerson(Person newPerson)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeletePerson(string lastName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEquatable<Person> GetPeople()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Person GetPerson(string lastName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdatePeople(IEquatable<Person> updatedPeople)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdatePerson(string lastName, Person updatedPerson)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
